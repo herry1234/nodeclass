07e71fc86fef9efa6273df4cc91eebd1d5d6a9e6
Hailiang Wang <hailiangwang@gmail.com>
2013-07-18 11:42:07 -0700

    mongo db

--- mongo.js
+++ mongo.js
@@ -1,14 +1,56 @@
-var util = require('util');
-var mongodb = require ('mongodb');
-var Db = mongodb.Db;
-var Connection = mongodb.Connection;
-var Server = mongodb.Server;
-var host = '127.0.0.1';
-var port = 27017;
-var db = new Db('test', new Server(host, port, {safe:false}));
-db.open(function(e, c) {
-	//do something with the database here
-	//console.log (util.inspect(db));
-	console.log(db._state);
-	db.close();
+// var util = require('util'),
+// 	mongodb = require('mongodb');
+// var Db = mongodb.Db;
+// var Connection = mongodb.Connection;
+// var Server = mongodb.Server;
+// var host = process.env.MONGO_DE_HOST;
+// var port = parseInt(process.env.MONGO_DE_PORT)
+// var db = new Db(process.env.MONGO_DE_DB, new Server(host, port, {
+// 	safe: false
+// }));
+// db.open(function(e, c) {
+// 	console.log(db._state);
+// 	db.close();
+// });
+
+var MONGOHQ_URL = 'mongodb://admin:admin@ds061747.mongolab.com:61747/herryassets';
+var mongodb = require('mongodb');
+var url = require('url');
+var log = console.log;
+var connectionUri = url.parse(MONGOHQ_URL);
+var dbName = connectionUri.pathname.replace(/^\//, '');
+mongodb.Db.connect(MONGOHQ_URL, function(error, client) {
+	if (error) throw error;
+	client.collectionNames(function(error, names) {
+		if (error) throw error;
+		//output all collection names
+		log("Collections");
+		log("===========");
+		var lastCollection = null;
+		names.forEach(function(colData) {
+			var colName = colData.name.replace(dbName + ".", '') ;
+			log(colName);
+			lastCollection = colName;
+		});
+		var collection = new mongodb.Collection(client, lastCollection);
+		log("\nDocuments in " + lastCollection);
+		var documents = collection.find({}, {
+			limit: 5
+		});
+		//output a count of all documents found
+		documents.count(function(error, count) {
+			log(" " + count + " documents(s) found");
+			log("====================");
+			// output the first 5 documents
+			documents.toArray(function(error, docs) {
+				if (error) throw error;
+				docs.forEach(function(doc) {
+					log(doc);
+				});
+				// close the connection
+				client.close();
+			});
+		});
+	});
 });
+
--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 var http = require('http');
 var port = process.env.PORT || 1337;
-var server = http.createServer(function(req, res) {
-	res.writeHead(200, {
-		'Content-Type': 'text/plain'
+var MONGOHQ_URL = 'mongodb://admin:admin@ds061747.mongolab.com:61747/herryassets';
+var mongodb = require('mongodb');
+var Db = mongodb.Db;
+var Connection = mongodb.Connection;
+var Server = mongodb.Server;
+var dbConnUrl = MONGOHQ_URL;
+var dbHost = url.parse(dbConnUrl).hostname;
+var dbPort = new Number(url.parse(dbConnUrl).port);
+console.log(dbHost + dbPort);
+var db = new Db('test', new Server(dbHost, dbPort, {}));
+db.open(function(e, c) {
+	// console.log (util.inspect(db));
+	// creates server
+	var server = http.createServer(function(req, res) {
+		//sets the right header and status code
+		res.writeHead(200, {
+			'Content-Type': 'text/plain'
+		});
+		//outputs string with line end symbol
+		res.end("MongoDB connected: " + db._state);
 	});
-	res.end('Hello nodejs');
-});
-
-server.listen(port, function() {
-	console.log("server running at %s:%s ",
-		server.address().address, server.address().port)
+	//sets port and IP address of the server
+	server.listen(port, function() {
+		console.log(
+			'Server is running at %s:%s ',
+			server.address().address,
+			server.address().port);
+	});
+	db.close();
 });
